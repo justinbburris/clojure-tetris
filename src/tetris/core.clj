86a9b0098fadea65380e04ca1db23408cd5e81d3
Justin Burris <jburris@vitrue.com>
2011-09-11 23:26:22 -0400

    Initial app creation with leniningen

---
+++ src/tetris/core.clj
@@ -0,0 +1,1 @@
+(ns tetris.core)
---
+++ test/tetris/test/core.clj
@@ -0,0 +1,6 @@
+(ns tetris.test.core
+  (:use [tetris.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
