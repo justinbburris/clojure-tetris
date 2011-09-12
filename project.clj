1a4262262db7d5cfee428f5a9026edad27dec59b
Justin Burris <jburris@vitrue.com>
2011-09-11 23:42:31 -0400

    Initial project settings. Guesses on what they mean

--- project.clj
+++ project.clj
@@ -1,3 +1,14 @@
+; I guess we're defining all of the app dependeices here to be loaded for the rest of the project
+; -----
+; I'm not totally sure why we state 1.0.0-SNAPSHOT - Perhaps as a means for defining a project version?
+; Why include SNAPSHOT though?
+; -----
+; I suppose that the main portion references tetris/core.clj
 (defproject tetris "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "Tetris implementation in clojure"
+  :dependencies [	[org.clojure/clojure "1.2.0"]
+									[org.clojure/clojure-contrib "1.2.0"]
+									[deflayout "0.9.0-SNAPSHOT"]]
+	:dev-dependencies [[swank-clojure "1.2.1"]]
+	:main tetris.core
+)
