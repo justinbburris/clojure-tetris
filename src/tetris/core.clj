66f6ba5c3105d4fe391c5a5ad13785dbb7d094c1
Justin Burris <jburris@vitrue.com>
2011-09-12 00:00:51 -0400

    Initial imports and blocks for the game. Including some other things

--- src/tetris/core.clj
+++ src/tetris/core.clj
@@ -1,1 +1,63 @@
-(ns tetris.core)
+; Okay definately java. Woah. Unexpected in this sort of environment.
+
+; More import statements - Looks like we have (namespace?) level depenedencies
+; Not sure what the exact difference is between import and use.
+; I guess use is for the things we included in the project clj?
+
+; gen-class?
+
+; import-static?
+
+(ns tetris.core
+	(:import 
+		(java.awt Color Dimension BorderLayout)
+		(javax.swing JPanel JFrame JOptionPane JButton JLabel)
+		(javax.awt.event KeyListener)
+	)
+	(:use
+		clojure.contrib.import-static deflayout core
+		clojure.contrib.swing-utils
+	)
+	(:gen-class)
+)
+
+(import-static java.awt.event.KeyEvent VK_LEFT VK_RIGHT VK_DOWN VK_UP VK_SPACE)
+
+; Tetris implementation
+(def empty-cell 0)
+(def moving-cell 1)
+(def filled-cell 2)
+(def glass-width 10)
+(def glass-height 20)
+(def zero-cords [3 0]) ; x,y
+
+; Tetris blocks 
+(def stick[	[0 0 0 0]
+						[1 1 1 1]
+						[0 0 0 0]
+						[0 0 0 0]])
+						
+(def square[[1 1]
+						[1 1]])
+
+(def tblock[[0 0 0]
+						[1 1 1]
+						[0 1 0]])
+
+(def sblock [	[0 1 0]
+							[0 1 1]
+							[0 0 1]])
+
+(def zblock [	[0 0 1]
+							[0 1 1]
+							[0 1 0]])
+
+(def lblock [	[1 1 0]
+							[0 1 0]
+							[0 1 0]])
+
+(def jblock [	[0 1 1]
+							[0 1 0]
+							[0 1 0]])
+							
+(def figures [stick square tblock sblock zblock lblock jblock])
